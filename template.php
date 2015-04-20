57d381b17ed2956ceffd004069556fea9d4823d1
Matt Barger <matthew_barger@hcpss.org>
2015-04-20 13:45:10 -0400

    Override page templates by content type

--- template.php
+++ template.php
@@ -14,3 +14,11 @@ function hcpss_schoolsite_theme_process_html(&$vars) {
     $vars['styles'] = null;
     $vars['scripts'] = null;
 }
+
+function hcpss_schoolsite_theme_preprocess_page(&$vars, $hook) {
+  if (isset($vars['node']->type)) {
+    // If the content type's machine name is "my_machine_name" the file
+    // name will be "page--my-machine-name.tpl.php".
+    $vars['theme_hook_suggestions'][] = 'page__' . $vars['node']->type;
+  }
+}
