14dd59cf6b25eda198f04412da98be713771c1a1
Matt Barger <matthew_barger@hcpss.org>
2015-04-27 16:16:26 -0400

    Add teaser template for news messages

---
+++ templates/node__news_message__teaser.tpl.php
@@ -0,0 +1,12 @@
+<div class="news-message">
+  <div class="news-message-image">
+    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="Logo image">
+  </div>
+  <div class="news-message-content">
+    <?php print render($title_prefix); ?>
+    <h1><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
+    <?php print render($title_suffix); ?>
+    <p class="news-message-detail">4/15/2015</p>
+    <p><?php print render($content); ?></p>
+  </div>
+</div>
