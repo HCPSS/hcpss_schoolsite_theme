977d49281bdf3e1ecec47114a5e71a3ffcd532f9
Matt Barger <matthew_barger@hcpss.org>
2015-04-27 10:26:14 -0400

    Create placeholder html and page templates

---
+++ templates/html.tpl.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <title><?php print $head_title; ?></title>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>High School Demo</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="http://code.cdn.mozilla.net/fonts/fira.css" >
+    <link href="stylesheets/index.css" rel="stylesheet" type="text/css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>
+    $(document).ready(function() {
+      if ($("#js-parallax-window").length) {
+        parallax();
+      }
+
+      var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
+      $('#js-centered-navigation-menu').removeClass("show");
+
+      menuToggle.on('click', function(e) {
+        e.preventDefault();
+        $('#js-centered-navigation-menu').slideToggle(function(){
+          if($('#js-centered-navigation-menu').is(':hidden')) {
+            $('#js-centered-navigation-menu').removeAttr('style');
+          }
+        });
+      });
+    });
+
+
+    </script>
+
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+
+<body class="index <?php print $classes; ?>" <?php print $attributes;?>>
+  <?php
+    print $page_top;
+    print $page;
+    print $page_bottom;
+  ?>
+</body>
+</html>
---
+++ templates/page.tpl.php
@@ -0,0 +1,96 @@
+<footer class="footer-2" role="contentinfo">
+
+      <div style="max-width: 68em; margin-left: auto; margin-right: auto;">
+      <div class="footer-logo">
+        <img src="images/marriottsridgehs.png" alt="Logo image">
+      </div>
+        <ul>
+          <li><a href="javascript:void(0)" style="font-size: 2em; color: white;">Marriotts Ridge High School</a></li>
+        </ul>
+
+        <div class="footer-secondary-links">
+          <ul>
+            <li><a href="javascript:void(0)">Contact Us</a></li>
+            <li><a href="javascript:void(0)">Search</a></li>
+          </ul>
+
+          <ul class="footer-social">
+            <li><a href="javascript:void(0)">
+              <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/twitter-logo-circle.png" alt="Twitter">
+            </a></li>
+            <li><a href="javascript:void(0)">
+              <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/facebook-logo-circle.png" alt="Facebook">
+              </a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+
+    <header class="centered-navigation" role="banner">
+      <div class="centered-navigation-wrapper">
+        <a href="javascript:void(0)" id="js-centered-navigation-mobile-menu" class="centered-navigation-mobile-menu">MENU</a>
+        <nav role="navigation">
+          <ul id="js-centered-navigation-menu" class="centered-navigation-menu show">
+            <li class="nav-link"><a href="javascript:void(0)">About</a></li>
+            <li class="nav-link"><a href="javascript:void(0)">News</a></li>
+            <li class="nav-link"><a href="javascript:void(0)">Calendar</a></li>
+            <li class="nav-link"><a href="javascript:void(0)">Our Staff</a></li>
+            <li class="nav-link"><a href="javascript:void(0)">Academics</a></li>
+            <li class="nav-link more"><a href="javascript:void(0)">Student Services</a>
+              <ul class="submenu">
+                <li><a href="javascript:void(0)">College Visits</a></li>
+                <li><a href="javascript:void(0)">Graduation Requirements</a></li>
+                <li><a href="javascript:void(0)">Scholarships</a></li>
+                <li><a href="javascript:void(0)">Tutoring</a></li>
+                <li><a href="javascript:void(0)">Work Permits</a></li>
+              </ul>
+            </li>
+            <li class="nav-link more"><a href="javascript:void(0)">Student Life</a>
+              <ul class="submenu">
+                <li><a href="javascript:void(0)">Athletics</a></li>
+                <li><a href="javascript:void(0)">Bell Schedules</a></li>
+                <li><a href="javascript:void(0)">Band</a></li>
+                <li><a href="javascript:void(0)">Activities &amp; Clubs</a></li>
+                <li><a href="javascript:void(0)">School Newspaper</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+<div class="refills-wrapper">
+  <div class="primary-content">
+    <?php print render($content); ?>
+  </div>
+
+  <footer class="footer" role="contentinfo">
+    <div class="footer-logo">
+      <!-- <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="Logo image"> -->
+      <img src="http://www.hcpss.org/f/mrb/base/hcpss-logo-outlined.png" alt="Logo image">
+    </div>
+    <div class="footer-links">
+      <ul>
+        <li><h3>Content</h3></li>
+        <li><a href="javascript:void(0)">About</a></li>
+        <li><a href="javascript:void(0)">Contact</a></li>
+        <li><a href="javascript:void(0)">Products</a></li>
+      </ul>
+      <ul>
+        <li><h3>Follow Us</h3></li>
+        <li><a href="javascript:void(0)">Facebook</a></li>
+        <li><a href="javascript:void(0)">Twitter</a></li>
+        <li><a href="javascript:void(0)">YouTube</a></li>
+      </ul>
+      <ul>
+        <li><h3>Content</h3></li>
+        <li><a href="javascript:void(0)">About</a></li>
+        <li><a href="javascript:void(0)">Contact</a></li>
+        <li><a href="javascript:void(0)">Products</a></li>
+      </ul>
+    </div>
+
+    <hr>
+
+    <p>Part of the Howard County Public School System</p>
+  </footer>
