42706ec9a19304788b4fd835429425b094d6a2fd
Matt Barger <matthew_barger@hcpss.org>
2015-04-28 09:57:31 -0400

    Fix $items loop

--- templates/fields/field--body.tpl.php
+++ templates/fields/field--body.tpl.php
@@ -44,10 +44,7 @@
  * @ingroup themeable
  */
 ?>
-<!--
-THIS FILE IS NOT USED AND IS HERE AS A STARTING POINT FOR CUSTOMIZATION ONLY.
-See http://api.drupal.org/api/function/theme_field/7 for details.
-After copying this file to your theme's folder and customizing it, remove this
-HTML comment.
--->
-<div><?php print render($item); ?></div>
+
+<?php foreach ($items as $delta => $item): ?>
+  <div><?php print render($item); ?></div>
+<?php endforeach; ?>
