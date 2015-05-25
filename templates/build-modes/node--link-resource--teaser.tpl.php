d5fb3d01f52efca63a2c701d45adbe7bd0564b8f
Matt Barger <matthew_barger@hcpss.org>
2015-05-25 13:03:01 -0400

    Adjust HTML for link resource teaser template

--- templates/build-modes/node--link-resource--teaser.tpl.php
+++ templates/build-modes/node--link-resource--teaser.tpl.php
@@ -1,4 +1,11 @@
 <div class="link-resource">
+  <div class="link-resource-image">
+    <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="Logo image">
+  </div>
+  <div class="link-resource-content">
+    <?php print render($title_prefix); ?>
     <div><?php print render($content['field_link__url']); ?></div>
+    <?php print render($title_suffix); ?>
     <div><?php print render($content['field_link_description']); ?></div>
+  </div>
 </div>
