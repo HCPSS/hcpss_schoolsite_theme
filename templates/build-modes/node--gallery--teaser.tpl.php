1fc88d604d08e7f8b4b27ab8ab51ebac353369f4
Matt Barger <matthew_barger@hcpss.org>
2015-05-26 03:37:15 -0400

    Add debug output

--- templates/build-modes/node--gallery--teaser.tpl.php
+++ templates/build-modes/node--gallery--teaser.tpl.php
@@ -1,5 +1,9 @@
+<?php
+  $hcpp = render($content);
+ ?>
+
 <a href="<?php print $node_url; ?>" class="grid-item grid-item-image">
   <i class="fa fa-trophy"></i>
   <h1><?php print $title; ?></h1>
-  <p><?php print render($content); ?></p>
+  <p> t- <?php print $hcpp; ?></p>
 </a>
