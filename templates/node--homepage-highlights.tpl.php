ce14cdd879d4bb9d0a88054ff59fa83b40a09b67
Matt Barger <matthew_barger@hcpss.org>
2015-05-26 01:07:32 -0400

    Override Homepage Highlights Node

---
+++ templates/node--homepage-highlights.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a node.
+ */
+
+?>
+
+<div class="line-behind-text"><h6>Important News &amp; Upcoming Events</h6></div>
+
+<div class="grid-items-lines">
+
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+    <div class="right-cover"></div>
+    <div class="bottom-cover"></div>
+</div>
