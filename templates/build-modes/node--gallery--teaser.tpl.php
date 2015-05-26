28c67cf27e42d7ad065a6f54b5da1ace1b61072c
Matt Barger <matthew_barger@hcpss.org>
2015-05-26 02:34:13 -0400

    Override teaser template for Galleries

---
+++ templates/build-modes/node--gallery--teaser.tpl.php
@@ -0,0 +1,5 @@
+<a href="<?php print $node_url; ?>" class="grid-item grid-item-image">
+  <i class="fa fa-trophy"></i>
+  <h1><?php print $title; ?></h1>
+  <p><?php print render($content); ?></p>
+</a>
