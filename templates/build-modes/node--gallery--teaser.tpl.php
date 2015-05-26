13a0df6078e7fbb99174b65d4a5d366361126280
Matt Barger <matthew_barger@hcpss.org>
2015-05-26 03:59:33 -0400

    Breakout values via XPath

--- templates/build-modes/node--gallery--teaser.tpl.php
+++ templates/build-modes/node--gallery--teaser.tpl.php
@@ -1,9 +1,19 @@
 <?php
-  $hcpp = render($content);
- ?>
+  $wrapped_content = "<div>" . render($content) . "</div>";
 
-<a href="<?php print $node_url; ?>" class="grid-item grid-item-image">
+  $xml = new SimpleXMLElement($wrapped_content);
+
+  $content_uri = $xml->xpath('//img/@src');
+  $content_taxonomy = $xml->xpath('span');
+  $content_description = $xml->xpath('/p');
+?>
+
+<a href="<?php print $node_url; ?>" class="grid-item"
+   style="background: url("<?php print $content_uri; ?>");
+          background-position: top;
+          background-repeat: no-repeat;
+          background-size: cover;">
   <i class="fa fa-trophy"></i>
   <h1><?php print $title; ?></h1>
-  <p> t- <?php print $hcpp; ?></p>
+  <p><?php print $content_description; ?></p>
 </a>
