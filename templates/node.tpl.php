76f7f0692f3d3e8ff1b2de06d660fe05cad2ee6b
Matt Barger <matthew_barger@hcpss.org>
2015-05-31 08:20:31 -0400

    Add action links to node template

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -24,6 +24,7 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
+      print render($action_links);
       print render($content);
     ?>
   </div>
