72455d8ed404cc005e237c00907f395fafba2a5d
Matt Barger <matthew_barger@hcpss.org>
2015-05-31 13:14:57 -0400

    Fixed preprocess hook function name

--- template.php
+++ template.php
@@ -1,6 +1,6 @@
 <?php
 
-function hcpss_schools_theme_preprocess_html(&$vars) {
+function hcpss_schoolsite_theme_preprocess_html(&$vars) {
   // Add the school code to the html.tpl.php template
   $vars['hcpss_school_name'] = variable_get('hcpss_school_code', 'generic');
 }
