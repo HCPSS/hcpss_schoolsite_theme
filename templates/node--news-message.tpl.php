1231ea7d039c25cdef7c168839b2ed151c5c3892
Matt Barger <matthew_barger@hcpss.org>
2015-06-01 01:23:26 -0400

    Override News Message template

---
+++ templates/node--news-message.tpl.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a node.
+ */
+
+?>
+
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <div class="submitted">
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php print $date; ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      // print render($action_links);
+      print render($content);
+    ?>
+  </div>
+
+</div>
