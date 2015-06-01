6f97e5d3aa50ad9125bf5a068f0a7bd18bb98124
Matt Barger <matthew_barger@hcpss.org>
2015-05-31 23:46:04 -0400

    Adjust fat footer

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -88,24 +88,51 @@
   </div>
   <div class="footer-links">
     <ul>
-      <li><h3>Content</h3></li>
-      <li><a href="javascript:void(0)">About</a></li>
-      <li><a href="javascript:void(0)">Contact</a></li>
-      <li><a href="javascript:void(0)">Products</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/news">News</a></li>
+      <li><a href="/calendar">Calendar</a></li>
+      <li><a href="/staff-list">Our Staff</a></li>
+      <li><a href="/">Academics</a></li>
     </ul>
     <ul>
-      <li><h3>Follow Us</h3></li>
-      <li><a href="javascript:void(0)">Facebook</a></li>
-      <li><a href="javascript:void(0)">Twitter</a></li>
-      <li><a href="javascript:void(0)">YouTube</a></li>
+      <li><h3>Student Services</h3></li>
+      <li><a href="javascript:void(0)">College Visits</a></li>
+      <li><a href="javascript:void(0)">Graduation Requirements</a></li>
+      <li><a href="javascript:void(0)">Scholarships</a></li>
+      <li><a href="javascript:void(0)">Tutoring</a></li>
+      <li><a href="javascript:void(0)">Work Permits</a></li>
     </ul>
     <ul>
-      <li><h3>Content</h3></li>
-      <li><a href="javascript:void(0)">About</a></li>
-      <li><a href="javascript:void(0)">Contact</a></li>
-      <li><a href="javascript:void(0)">Products</a></li>
+      <li><h3>Student Life</h3></li>
+      <li><a href="javascript:void(0)">Athletics</a></li>
+      <li><a href="javascript:void(0)">Activities and Clubs</a></li>
+      <li><a href="javascript:void(0)">School Newspaper</a></li>
+    </ul>
+    <ul>
+      <li><h3>Essential Applications</h3></li>
+      <li><a href="https://hcpss.me/fp/">Family Portal</a><br>Stay on top of your child’s grades and attendance</li>
+      <li><a href="http://launch.customschoolapp.net/hcpss/">HCPSS Mobile App</a><br>Aggregate news and events from the district and your school</li>
+      <li><a href="javascript:void(0)">Naviance</a><br>Identify colleges and careers that align with interests and goals</li>
+      <li><a href="https://hcpss2.securesites.net/SchoolLocator/index.jsp">School & Bus Locator</a><br>Find your child’s school and view bus pickup times</li>
+    </ul>
+    <ul>
+      <li><h3>HCPSS Resources</h3></li>
+      <li><a href="http://www.hcpss.org/about-us/">About HCPSS</a><br>Learn more about the school system</li>
+      <li><a href="http://www.hcpss.org/academics/">Curriculum & Academics</a><br>Get an overview of the HCPSS curriculum</li>
+      <li><a href="http://www.hcpss.org/schools/enrollment-and-registration/">Enrollment & Registration</a><br>How to enroll your child in the school system</li>
+      <li><a href="http://www.hcpss.org/food-services/">Food Services</a><br>View menus, meal prices, and make online payments</li>
+      <li><a href="http://www.hcpss.org/about-us/forms/">HCPSS Forms</a><br>Health, medication, athletic, and application forms</li>
+      <li><a href="http://www.hcpss.org/academics/testing/parcc/">PARCC Assessments</a><br>Learn more about the PARCC assessments</li>
+      <li><a href="http://www.hcpss.org/board/policies/">Policies and Procedures</a><br>Review the policies set by the Board of Education</li>
+      <li><a href="http://www.hcpss.org/about-us/handbook/">Student Handbook</a><br>Guidebook for expected student behavior</li>
+    </ul>
+    <ul>
+      <li><h3>Get Involved</h3></li>
+      <li><a href="http://www.lrhsptsa.org/">PTSA</a></li>
+      <li><a href="http://www.lrhsboosters.com/">Boosters</a></li>
     </ul>
   </div>
+
 
   <hr>
 
