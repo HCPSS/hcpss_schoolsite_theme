2c46e86311495cb499523973d9390bc7fdeee09c
Matt Barger <matthew_barger@hcpss.org>
2017-06-27 13:49:17 -0400

    Add Google Analytics
    Add the Google Analytics embed code.  The individual site’s property id
    is pulled from data provided via the hcpss_schoolsite_config module.

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -43,5 +43,18 @@
     print $page;
     print $page_bottom;
   ?>
+
+  <?php if ($analytics_id): ?>
+    <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+     ga('create', '<?php print $analytics_id; ?>', 'auto');
+     ga('send', 'pageview');
+    </script>
+  <?php endif; ?>
+
 </body>
 </html>
